f5331ff4d8df9319a65d85449b7eae9a7d082889
Logan King <59426296+EagleAim@users.noreply.github.com>
2023-11-01 21:42:25 -0700

    5 questions done

--- script.js
+++ script.js
@@ -7,17 +7,28 @@ let HelloWorld="Hello World!";
 console.log(HelloWorld);
 // 2. Write code to concatenate three strings and output the result to the console:
 // "Hello", " ", "World!"
-
+let Hello="Hello";
+let space=" "
+let World="World!";
+console.log(Hello, space, World);
 // 3. Write code to output "Hello" on one line and "World!" on the next line to the console.
 // *Note: You should only have one console.log() statement - how do you add a new line in
 // a string?...
+let Hel = `
+      Hello
+      World!.
+`;
+ 
+console.log(Hel);
 
 // 4. Write code to output the result of the arithmetic expression to the console:
 // 3 * 8 + 2;
+console.log(3 * 8 + 2) //26
 
 // 5. Write code to output the result of the comparison:
 // 5 is less than 10.
 
+console.log(10 > 5) //true
 // 6. Write code to output the result of the comparison:
 // 10 is greater than 5.
 
